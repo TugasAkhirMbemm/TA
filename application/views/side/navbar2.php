75cc41ae9e6c25937a868a4eb419ab61f84e16da
DESKTOP-FRO9QU4\Aprian-PC <rangga.aprian03@gmail.com>
2019-01-29 08:44:50 +0700

    uri segment error

--- application/views/side/navbar2.php
+++ application/views/side/navbar2.php
@@ -7,21 +7,31 @@
 
     <nav id="nav-menu-container">
       <ul class="nav-menu">
-        <li class="menu-active"><a href="<?php echo base_url('Data_siswa') ?>">Data Diri Siswa</a></li>
-        <li class="menu-active"><a href="<?php echo base_url('Upload_DU/UploadDaftarUlang') ?>">Upload</a></li>
+        <li>
+          <a class="<?php if($this->uri->segment('1')=="Data_siswa"){ echo "active";}?>" href="<?php echo base_url('Data_siswa') ?>">Data Diri Siswa
+          </a>
+        </li>
+
+        <li>
+          <a class="Upload_DU" href="<?php echo base_url('Upload_DU/UploadDaftarUlang') ?>">Upload</a>
+        </li>
        
-        <li class="menu-has-children <?php if($this->uri->segment('1')=="PPDB"){ echo "active";}?>" href="#" ><a href="#">Jadwal</a>
+        <li class="menu-has-children" href="#" ><a class="<?php if($this->uri->segment('1')=="Jadwal"){ echo "active";}?>" href="#">Jadwal</a>
           <ul>
             <li><a href="<?php echo base_url('Jadwal/JadwalTestTK') ?>">Jadwal Tes TK</a></li>
             <li><a href="<?php echo base_url('Jadwal/JadwalTestSD') ?>">Jadwal Tes SD</a></li>
             <li><a href="<?php echo base_url('Jadwal/JadwalTestSMP') ?>">Jadwal Tes SMP</a></li>
           </ul>
         </li>
-        <li class="menu-has-children <?php if($this->uri->segment('1')=="Pengumuman"){ echo "active";}?>" href="#" ><a href="#">Pengumuman</a>
+        
+        <li class="menu-has-children" href="#" >
+          <a class="<?php if($this->uri->segment('1')=="Pengumuman"){ echo "active";}?>" href="#">Pengumuman</a>
           <ul>
             <li><a href="<?php echo base_url('Pengumuman/Peng_Awal') ?>">Pengumuman awal</a></li>
             <li><a href="<?php echo base_url('Pengumuman/Peng_Akhir') ?>">Pengumuman akhir</a></li>
           </ul>
+        </li>
+
         <li><a href="<?php echo base_url('Home') ?>">Logout</a></li>
       </nav>
     </div>
