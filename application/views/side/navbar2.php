0cb779def18618693eb4a207d6b5e9958d9cf170
DESKTOP-FRO9QU4\Aprian-PC <rangga.aprian03@gmail.com>
2019-01-29 08:48:13 +0700

    jij

--- application/views/side/navbar2.php
+++ application/views/side/navbar2.php
@@ -1,21 +1,17 @@
 <header id="header">
   <div class="container">
-
     <div id="logo" class="pull-left">
       <a href="#head"><img style="margin-top: -10px" src="<?php echo base_url() ?>master/assets/img/new1.png" alt="" title="" /></img></a>
     </div>
-
     <nav id="nav-menu-container">
       <ul class="nav-menu">
         <li>
           <a class="<?php if($this->uri->segment('1')=="Data_siswa"){ echo "active";}?>" href="<?php echo base_url('Data_siswa') ?>">Data Diri Siswa
           </a>
         </li>
-
         <li>
           <a class="Upload_DU" href="<?php echo base_url('Upload_DU/UploadDaftarUlang') ?>">Upload</a>
         </li>
-       
         <li class="menu-has-children" href="#" ><a class="<?php if($this->uri->segment('1')=="Jadwal"){ echo "active";}?>" href="#">Jadwal</a>
           <ul>
             <li><a href="<?php echo base_url('Jadwal/JadwalTestTK') ?>">Jadwal Tes TK</a></li>
@@ -23,7 +19,6 @@
             <li><a href="<?php echo base_url('Jadwal/JadwalTestSMP') ?>">Jadwal Tes SMP</a></li>
           </ul>
         </li>
-        
         <li class="menu-has-children" href="#" >
           <a class="<?php if($this->uri->segment('1')=="Pengumuman"){ echo "active";}?>" href="#">Pengumuman</a>
           <ul>
@@ -31,13 +26,8 @@
             <li><a href="<?php echo base_url('Pengumuman/Peng_Akhir') ?>">Pengumuman akhir</a></li>
           </ul>
         </li>
-
         <li><a href="<?php echo base_url('Home') ?>">Logout</a></li>
-      </nav>
-    </div>
-  </header>
-
-</div>
-</div>
-</div>
-</div>
+      </ul>
+    </nav>
+  </div>
+</header>
